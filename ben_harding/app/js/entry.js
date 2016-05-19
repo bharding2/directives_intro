6c1d0e137b4ec1a3d41ec4d41857c816f4c6691e
bharding2 <b.harding@gmail.com>
2016-05-19 10:50:52 -0700

    removing dead code

--- ben_harding/app/js/entry.js
+++ ben_harding/app/js/entry.js
@@ -24,15 +24,3 @@ fakeApp.controller('TeamsController', function() {
     { name: 'Gaspar Duran', position: 'Wingback' }
   ];
 });
-
-// fakeApp.run(['$rootScope', function($rootScope) {
-//   $rootScope.players = [
-//     { name: 'Normand Harvey', position: 'Inner Midfielder' },
-//     { name: 'Izcautzin Arizmendi', position: 'Winger' },
-//     { name: 'Bernard Boulanger', position: 'Forward' },
-//     { name: 'Elwood Finch', position: 'Withdrawn Forward' },
-//     { name: 'Nikos Galanos', position: 'Forward' },
-//     { name: 'Roger Wilcox', position: 'Attacking Midfielder' },
-//     { name: 'Randy Baribeau', position: 'Winger' }
-//   ];
-// }]);
